c48988fcc890e3122f0ec8b1d9e3ed9bd7f7042a
andref <95976799+nacchip@users.noreply.github.com>
2024-04-26 20:24:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return "hi"
+
+@app.route('/about')
+def home():
+    return "about"
 
 app.run(debug=True)
